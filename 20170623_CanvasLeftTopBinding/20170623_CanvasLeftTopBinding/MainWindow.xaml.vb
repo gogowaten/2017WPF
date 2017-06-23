47f9236f0e9204b3d930517b7bd6983f1cb005f9
waten <gogowaten@yahoo.co.jp>
2017-06-24 01:50:47 +0900

    add dependency property

--- 20170623_CanvasLeftTopBinding/20170623_CanvasLeftTopBinding/MainWindow.xaml.vb
+++ 20170623_CanvasLeftTopBinding/20170623_CanvasLeftTopBinding/MainWindow.xaml.vb
@@ -1,15 +1,41 @@
-﻿
+﻿'WPF、CanvasLeftTopとSliderValueをBinding ( ソフトウェア ) - 午後わてんのブログ - Yahoo!ブログ
+'https://blogs.yahoo.co.jp/gogowaten/14988621.html
+
 
 
 Class MainWindow
     '右へ10
     Private Sub AddLeft10()
-        Canvas.SetLeft(MyBorder, Canvas.GetLeft(MyBorder) + 10)
+        'Canvas.SetLeft(MyBorder, Canvas.GetLeft(MyBorder) + 10)
+        MyLeft += 10
     End Sub
     '左へ10
     Private Sub SubLeft10()
-        Canvas.SetLeft(MyBorder, Canvas.GetLeft(MyBorder) - 10)
+        'Canvas.SetLeft(MyBorder, Canvas.GetLeft(MyBorder) - 10)
+        MyLeft -= 10
     End Sub
+
+    Public Shared ReadOnly MyLeftProperty As DependencyProperty = DependencyProperty.Register(
+        NameOf(MyLeft), GetType(Double), GetType(Border), New PropertyMetadata(0.0))
+    Public Property MyLeft As Double
+        Get
+            Return GetValue(MyLeftProperty)
+        End Get
+        Set(value As Double)
+            SetValue(MyLeftProperty, value)
+        End Set
+    End Property
+    Public Shared ReadOnly MyTopProperty As DependencyProperty = DependencyProperty.Register(
+        NameOf(MyTop), GetType(Double), GetType(Border), New PropertyMetadata(0.0))
+    Public Property MyTop As Double
+        Get
+            Return GetValue(MyTopProperty)
+        End Get
+        Set(value As Double)
+            SetValue(MyTopProperty, value)
+        End Set
+    End Property
+
 
     'バインディング作成用
     Private Function GetMyBinding(sObj As DependencyObject, sDp As DependencyProperty, strF As String) As Binding
@@ -27,10 +53,19 @@ Class MainWindow
         AddHandler btSub10.Click, AddressOf SubLeft10
 
         'バインディング
-        Dim bindL As Binding = GetMyBinding(MyBorder, Canvas.LeftProperty, "CanvasLeft = {0:0.0}")
+        'Dim bindL As Binding = GetMyBinding(MyBorder, Canvas.LeftProperty, "CanvasLeft = {0:0.0}")
+        'sldCanvasLeft.SetBinding(Slider.ValueProperty, bindL)
+        'tbCanvasLeft.SetBinding(TextBlock.TextProperty, bindL)
+        'Dim bindT As Binding = GetMyBinding(MyBorder, Canvas.TopProperty, "CanvasTop = {0:0.0}")
+        'sldCanvasTop.SetBinding(Slider.ValueProperty, bindT)
+        'tbCanvasTop.SetBinding(TextBlock.TextProperty, bindT)
+
+        Dim bindL As Binding = GetMyBinding(Me, MyLeftProperty, "MyLeft = {0:0.0}")
+        MyBorder.SetBinding(LeftProperty, bindL)
         sldCanvasLeft.SetBinding(Slider.ValueProperty, bindL)
         tbCanvasLeft.SetBinding(TextBlock.TextProperty, bindL)
-        Dim bindT As Binding = GetMyBinding(MyBorder, Canvas.TopProperty, "CanvasTop = {0:0.0}")
+        Dim bindT As Binding = GetMyBinding(Me, MyTopProperty, "MyTop = {0:0.0}")
+        MyBorder.SetBinding(TopProperty, bindT)
         sldCanvasTop.SetBinding(Slider.ValueProperty, bindT)
         tbCanvasTop.SetBinding(TextBlock.TextProperty, bindT)
     End Sub
